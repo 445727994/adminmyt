89db47d0041c46843c7eb2b249f8762152d9b542
yhcg480 <445727994>
2019-11-19 15:32:40 +0800

    麦芽学院

--- app/Admin/Controllers/MarketsSchoolController.php
+++ app/Admin/Controllers/MarketsSchoolController.php
@@ -19,14 +19,13 @@ class MarketsSchoolController extends AdminController {
 	 */
 	protected function grid() {
 		$grid = new Grid(new Market);
-		$grid->model()->where('cate_pid', '=', 4);
+		$grid->model()->where('cate_pid', '=', 6);
 		$grid->column('id', __('Id'));
-		$grid->column('content', __('内容'));
-		$grid->column('images', __('图片'))->image('', 100, 100);
+		$grid->column('item_title', __('标题'));
+		$grid->column('images', __('封面图'))->image('', 100, 100);
 		$grid->column('shares', __('分享次数'));
 		//$grid->column('type', __('Type'));
 		$grid->column('cate.name', __('分类')); //
-
 		$grid->column('created_at', __('创建时间'));
 		$grid->column('updated_at', __('更新时间'));
 		return $grid;
@@ -42,12 +41,11 @@ class MarketsSchoolController extends AdminController {
 		$show = new Show(Market::findOrFail($id));
 
 		$show->field('id', __('Id'));
-
-		$show->field('images', __('图片'));
+		$show->field('images', __('封面图'));
 		$show->field('shares', __('分享次数'));
 		$show->field('created_at', __('创建时间'));
 		$show->field('updated_at', __('更新时间'));
-		$show->field('type', __('Type'));
+		$show->field('url', __('https网址'));
 
 		return $show;
 	}
@@ -59,15 +57,15 @@ class MarketsSchoolController extends AdminController {
 	 */
 	protected function form() {
 		$form = new Form(new Market);
-
 		//  $form->switch('type', __('Type'))->default(1);
-		$form->select('cate_id', __('分类'))->options(MarketCate::selectOption(['pid' => 4], false))->help('选择分类')->rules('required', ['required' => '请选择分类']);
+		$form->text("item_title", '标题');
+		$form->image('images', __('封面图'))->uniqueName(); //
+		$form->select('cate_id', __('分类'))->options(MarketCate::selectOption(['pid' => 6], false))->help('选择分类')->rules('required', ['required' => '请选择分类']);
+		$form->text('url', __('https网址'));
+		$form->text('content', __('描述'))->help("内容简介！");
 		$form->number('shares', __('分享次数'));
-		$form->hidden('cate_pid')->default(4);
-		$form->Image('images', __('封面图'))->uniqueName()->sortable()->removable();
-		$form->kindeditor('content', __('标题'))->help("请勿在内容中上传图片！");
+		$form->hidden('cate_pid')->default(6);
 		//$form->number('is_jx', __('Is jx'));
-
 		return $form;
 	}
 }
